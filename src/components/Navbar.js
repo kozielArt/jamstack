303dd7601f4012efa89a45a9c291954e53956e8f
Artur Kozieł <arturkoziel@macwaw002.local>
2018-12-13 11:29:19 +0100

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/veronym-log.png'
 
 const Navbar = class extends React.Component {
 
@@ -31,11 +31,11 @@ const Navbar = class extends React.Component {
  render() {
    return (
   
-  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
+  <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation"  style={{ minHeight: '5.25rem', background: '#004d9b' }}>
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          <img src={logo} alt="Kaldi" style={{  height: '200px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -45,32 +45,24 @@ const Navbar = class extends React.Component {
         </div>
       </div>
       <div id="navMenu" className="navbar-menu">
-      <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
       <div className="navbar-end has-text-centered">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+        <Link className="navbar-item" to="/about"  style={{ color: '#fff' }}>
+          What is Veronym
+        </Link>
+        <Link className="navbar-item" to="/products"  style={{ color: '#fff' }}>
+          How it works
+        </Link>
+        <Link className="navbar-item" to="/contact"  style={{ color: '#fff' }} >
+          Pricing
+        </Link>
+        <Link className="navbar-item" to="/contact/examples"  style={{ color: '#fff' }}>
+          Sign in
+        </Link>
+        <Link className="navbar-item" to="localhost:3000/register"  style={{ color: '#fff', fontWeight: '900' }}>
+          Start free Trial
+        </Link>
       </div>
+
       </div>
     </div>
   </nav>
